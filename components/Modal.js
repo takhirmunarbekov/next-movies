923db0e0a9e2244c1b8b61ec639fb88c7c2853b4
Takhir Munarbekov <admin@tami.kz>
2020-10-04 10:08:16 +0600

    Feat: Modal Connect

--- components/Modal.js
+++ components/Modal.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react'
 
+const YOUTUBE_LINK = 'https://www.youtube.com/embed/'
+
 const Modal = ({ items = [], type = 'image', isOpen, currentItem, close }) => {
   const [activeItem, setActiveItem] = useState(currentItem || 0)
+
+  const changeItem = step => {
+    if (step + activeItem >= 0 && step + activeItem < items.length) {
+      setActiveItem(step + setActiveItem)
+    }
+  }
 
   return isOpen ? (
     <div className="modal">
@@ -10,8 +18,24 @@ const Modal = ({ items = [], type = 'image', isOpen, currentItem, close }) => {
         <img src="/close.svg" alt="" />
       </div>
       <div className="modal__inner">
-        <button className="modal__button modal__prev"></button>
-        <button className="modal__button modal__next"></button>
+        <button className="modal__button modal__prev" onClick={() => changeItem(-1)}>
+          <img src="/arrow-left.svg" alt="" />
+        </button>
+        <div className="modal__content">
+          {type === 'image' ? <img src={items[activeItem]} alt="" /> : null}
+          {type === 'video' ? (
+            <iframe
+              title={items[activeItem].key}
+              src={`${YOUTUBE_LINK + items[activeItem].key}?rel=0&showinfo=0&autoplay=1`}
+              allowFullScreen="allowfullscreen"
+              allow="autoplay; encrypted-media"
+              frameBorder="0"
+            />
+          ) : null}
+        </div>
+        <button className="modal__button modal__next" onClick={() => changeItem(1)}>
+          <img src="/arrow-right.svg" alt="" />
+        </button>
       </div>
     </div>
   ) : null
--- components/Videos.js
+++ components/Videos.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal'
 
-const YOUTUBE_LINK = 'https://www.youtube.com/embed/'
+const Videos = ({ videos = [] }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [currentItem, setCurrentItem] = useState(0)
 
-const Videos = ({ videos = [] }) => (
-  <div className="videos">
-    {videos.map(video => (
-      <div className="videos__item">
-        <img src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`} alt="" />
-        <span>
-          <img src="/play.svg" alt="" />
-        </span>
-      </div>
-    ))}
-  </div>
-)
+  const openModal = key => {
+    setCurrentItem(key)
+    setIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <div className="videos">
+      {videos.map((video, key) => (
+        <div key={video.id} className="videos__item" onClick={() => openModal(key)}>
+          <img src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`} alt="" />
+          <span>
+            <img src="/play.svg" alt="" />
+          </span>
+        </div>
+      ))}
+      <Modal
+        isOpen={isOpen}
+        currentItem={currentItem}
+        close={closeModal}
+        items={videos}
+        type="video"
+      />
+    </div>
+  )
+}
 
 export default Videos
